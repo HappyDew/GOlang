6489c6b62d2bb7e25af7d61fd88853e473c5b476
HappyDew <mygpec@mail.ru>
2018-06-20 11:49:06 +0900

    The end

--- lesson.go
+++ lesson.go
@@ -59,22 +59,25 @@
 // }
 
 package main
-import ("fmt"
-  "sort")//добавляется пакет
 
-func main(){
-  x := []int{
-    48,96,86,68,
-    57,82,63,70,
-    37,34,83,27,
-    19,97,9,17,
-   }
-  // min := x[0]
-  // for i := 0; i < len(x); i++ {
-  //   if min > x[i] {
-  //     min = x[i]
-  //   }
-  // }
-  sort.Ints(x)
-  fmt.Println(/*min*/ x[0])
+import (
+	"fmt"
+	"sort"
+) //добавляется пакет
+
+func main() {
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+	// min := x[0]
+	// for i := 0; i < len(x); i++ {
+	//   if min > x[i] {
+	//     min = x[i]
+	//   }
+	// }
+	sort.Ints(x)
+	fmt.Println( /*min*/ x[0])
 }
